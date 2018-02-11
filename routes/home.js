39f1700e4ba398705362749de71317c754a50125
Marcos García <marcosluisgm@gmail.com>
2018-02-11 21:01:42 +0100

    Add devices endpoint and open endpoint

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var devices = require('../config').devices;
 
-router.get('/', function(req, res, next) {
 
-    var status ={
-        status: "OPEN"
-    }
+/**
+ * Return a list of devices with its configuration
+ */
+router.get('/devices', function (req, res, next) {
+    res.json(devices)
+});
 
-  res.json(status);
+/**
+ * Open specific device
+ */
+router.get('/open/:key', function(req, res, next) {
+    // Get the device by the provided key
+    var config = devices[req.params.key];
+
+    // Device not found
+    if (!config) return res.json({ status: 'ERROR', message: 'Key not found' });
+
+    request.get(`http://${config.ip}:${config.port}/open`, function (_err, _res, _body) {
+        // Error response
+        if (_err) return res.json({ status: 'ERROR' });
+
+        // Successful response
+        return res.json(JSON.parse(_body));
+    })
 });
 
 module.exports = router;
